eaae5101640d473dae65f2f9a8f99a0a92744232
Paul Chrysis <paulchrysis@Pauls-MacBook-Pro.local>
2024-03-26 18:30:05 +0200

    Added payment_id, currency_code, invoice_url to payment

--- lib/brooqly_db_engine/models/payments.rb
+++ lib/brooqly_db_engine/models/payments.rb
@@ -5,12 +5,15 @@ module BrooqlyDbEngine
         table name: :payments, key: :id, capacity_mode: :on_demand
 
         field :store_id, :string
+        field :payment_id, :string
         field :transaction_id, :string
         field :treats_id, :array, of: :string
         field :amount, :string
+        field :currency_code, :string
         field :status, :string
         field :date, :string
         field :invoice_no, :string
+        field :invoice_url, :string
         global_secondary_index hash_key: :store_id
         def self.have_status(status)
             # Return how many treats are purchased (so not consumed and paid)
